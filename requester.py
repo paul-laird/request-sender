3bd726eb67775caba98b289c49d0d70aeec35271
paul-laird <35932444+paul-laird@users.noreply.github.com>
2025-11-14 12:53:24 +0000

    Update requester.py fix

--- requester.py
+++ requester.py
@@ -7,6 +7,6 @@ with open('/var/www/html/request.log') as f:
   for i in range(10000):
     resp = requests.post(url, data=data + str(i))
     time.sleep(15)
-    f.write( data=data + str(i)+',')
+    f.write(data + str(i)+',')
     f.write(f"Status Code: {resp.status_code}"+',')
     f.write(f"Response Text: {resp.text}"+'\n')
