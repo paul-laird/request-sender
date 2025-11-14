b5784093d617a029bd95a5756255f7277378bc82
paul-laird <35932444+paul-laird@users.noreply.github.com>
2025-11-14 14:38:55 +0000

    Update requester.py
    convert to function - to be improved

--- requester.py
+++ requester.py
@@ -1,15 +1,29 @@
 import sys
 import requests
 import time
-url = 'https://helloworld.requestcatcher.com/'+sys.argv[2]
-data = f'{sys.argv[3]}! '
+import argparse
+import json
 
-with open(sys.argv[1],'w') as f:
-  for i in range(10000):
-    resp = requests.post(url, data=data + str(i))
-    time.sleep(15)
-    f.write(data + str(i)+',')
-    f.write(f"Status Code: {resp.status_code}"+',')
-    f.write(f"Response Text: {resp.text}"+'\n')
-    if not i%10:
-      f.flush()
+url = sys.argv[2]
+data = sys.argv[3]
+num= int(sys.argv[4])
+interval=int(sys.argv[5])
+fn=sys.argv[1]
+parser = argparse.ArgumentParser(description='A simple script demonstrating argparse with defaults.')
+parser.add_argument('--var', type=str, default='page', help='The element of json data that will increment.')
+args = parser.parse_args()
+var=args.var
+
+def sendRequests(fn,url,data,num,interval,var):
+  with open(fn,'w') as f:
+    for i in range(num):
+      j=json.loads(data)
+      j[var]=i
+      data=json.dumps(j)
+      resp = requests.post(url, data=data)
+      time.sleep(15)
+      f.write(data + str(i)+',')
+      f.write(f"Status Code: {resp.status_code}"+',')
+      f.write(f"Response Text: {resp.text}"+'\n')
+      if not i%10:
+        f.flush()
